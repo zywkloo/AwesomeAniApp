f8b361820d42eb27a3eff5420fa5aee7e085e955
Yiwei Zhang <zywkloo@student.ubc.ca>
2024-03-26 13:07:30 -0700

    Add a counter toy

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react'; // Import useState from react
+
 
 export default function App() {
+  const [count,setCount] = useState(0)
+  // const [values,set] = useEffect([])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{count}</Text>
+      <TouchableOpacity onPress={()=>setCount(count+1)}>
+        <Text>Add counts</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
